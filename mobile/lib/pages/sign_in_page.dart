ee83e7eef9519bc29042a4bf9e89d00f919a9014
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-04 15:04:05 +0700

    add theme & splash page

---
+++ mobile/lib/pages/sign_in_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Sign In Page'),
+      ),
+    );
+  }
+}
