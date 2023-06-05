a07fc82781f18ac0d713f0e986c300c18f801f10
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-05 19:09:17 +0700

    slicing signin page

--- mobile/lib/pages/sign_in_page.dart
+++ mobile/lib/pages/sign_in_page.dart
@@ -1,13 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    Widget header(){
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Login', style: primaryTextStyle.copyWith(fontSize: 24,fontWeight: semiBold,),),
+            SizedBox(height: 2,),
+            Text('Sign In to Continue', style: subtitleTextStyle,)
+          ],
+        ),
+      );
+    }
+
+    Widget passwordInput(){
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Password", style: primaryTextStyle.copyWith(
+              fontSize: 16, fontWeight: medium,
+            ),),
+            SizedBox(height: 12,),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16,),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset('assets/icon_password.png',width: 17,),
+                    SizedBox(width: 16,),
+                    Expanded(child: TextFormField(
+                      style: primaryTextStyle,
+                      decoration: InputDecoration.collapsed(hintText: "Your Email Address", hintStyle: subtitleTextStyle),
+                    ),),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget emailInput(){
+      return Container(
+        margin: EdgeInsets.only(top: 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Email Address", style: primaryTextStyle.copyWith(
+              fontSize: 16, fontWeight: medium,
+            ),),
+            SizedBox(height: 12,),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 16,),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset('assets/icon_email.png',width: 17,),
+                    SizedBox(width: 16,),
+                    Expanded(child: TextFormField(
+                      obscureText: true,
+                      style: primaryTextStyle,
+                      decoration: InputDecoration.collapsed(hintText: "Your Password", hintStyle: subtitleTextStyle),
+                    ),),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget signInButton(){
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(onPressed: (){}, child: Text("Sign In", style: primaryTextStyle.copyWith(
+          fontSize: 16,
+          fontWeight: medium,
+        ),),
+        style: TextButton.styleFrom(backgroundColor: primaryColor, shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        )),
+        ),
+      );
+    }
+
+    Widget footer(){
+      return Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Don\'t have an account? ',
+            style: subtitleTextStyle.copyWith(
+              fontSize: 12,
+            ),
+            ),
+            Text("Sign Up", style: purpleTextStyle.copyWith(
+              fontSize: 12,
+              fontWeight: medium,
+            ),),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Sign In Page'),
+      backgroundColor: backgroundColor1,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              emailInput(),
+              passwordInput(),
+              signInButton(),
+              Spacer(),
+              footer(),
+            ],
+          ),
+        ),
       ),
     );
   }
