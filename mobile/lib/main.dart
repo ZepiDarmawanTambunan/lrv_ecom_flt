487c4ccb908e38fe13b0bc367fe091aab34e33da
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-12 18:36:13 +0700

    setup firebase core & cloud firestore

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -14,8 +14,14 @@ import 'package:mobile/providers/product_provider.dart';
 import 'package:mobile/providers/transaction_provider.dart';
 import 'package:mobile/providers/wishlist_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main () => runApp(const MyApp());
+void main ()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
